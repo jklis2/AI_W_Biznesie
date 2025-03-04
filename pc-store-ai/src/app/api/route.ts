3adca859bc38e7228f3f51fa527464c7ddb42159
Jakub Kliś <jakubklis2201@gmail.com>
2025-03-04 11:34:44 +0100

    feat: Add MongoDB connection

--- pc-store-ai/src/app/api/route.ts
+++ pc-store-ai/src/app/api/route.ts
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 
-export function GET() {
-    return NextResponse.json({ message: "Server is running" });
+export async function GET() {
+    const mongoUri: string | undefined = process.env.MONGODB_URI;
+
+    if (!mongoUri) {
+        return NextResponse.json({ message: "MONGODB_URI is not set in environment variables" }, { status: 500 });
+    }
+
+    try {
+        await mongoose.connect(mongoUri);
+
+        return NextResponse.json({ message: "Server is running", db: "Connected to MongoDB" });
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        return NextResponse.json({ message: "Server is running", db: "Failed to connect to MongoDB" }, { status: 500 });
+    }
 }
